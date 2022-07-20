1b7298db443f1a950e6573b2ab0f9f8293be76bd
Furhan Samani <contact@furhan.dev>
2022-07-19 17:01:56 -0700

    update server.js, connection.js to hookup sequelize and start express

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const { Post, User } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/', async (req, res) => {
+});
+
+module.exports = router;
